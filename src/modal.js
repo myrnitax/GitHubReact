6b88f1249452d132f496c83876ae97806b348b07
Myrna <myrna.alvarado.ve@gmail.com>
2023-03-09 11:18:03 +0100

    Componentes Basados en clases

--- src/modal.js
+++ src/modal.js
@@ -1,36 +1,76 @@
-import { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import Overlay from './overlay'
-import styled  from 'styled-components'
-import {ButtonContrast} from './components/button'
+import styled from 'styled-components'
+import { ButtonContrast } from './components/button'
 import InputText from './components/input-text'
 
-const ModalContentStyled = styled.form`
-position: fixed;
-inset-block-start: 50%;
-transform: translateY(-50%);
-inset-inline-start: 50%;
-transform: translateX(-50%);
-background: var(--bg);
-color: var(--white);
-padding: 1.5rem;
-border-radius: 0.5rem;
-display: flex;
-flex-direction: column;
-gap: 1rem;
-inline-size: 24rem;
-.title{
-  font: var(--headline2-semibold);
-  margin: 0;
+class Modal extends React.Component {
+  constructor(props) {
+    super(props)
+    this.name = 'asfsafdasdsadasdsadsa'
+  }
+  state = {
+    a: 'Miguel'
+  }
+
+  componentDidUpdate() { //este llamaria cada vez que se actualiza la pagina por completo
+    console.log('el componente se actualizó')
+  }
+  componentWillUnmount() {
+    console.log('el componente está a punto de desaparecer')
+  }
+  componentDidMount() { //esto solo llama cuando ya se renderio por primera vez
+    setTimeout(() => {
+
+      this.setState({
+        a: 'Leo',
+        b: this.name
+      })
+    }, 5000)
+
+    // this.setState({
+    //   b: ''
+    // })
+  }
+  render() {
+    return (
+      <div style={{ background: 'black' }}>
+        {this.state.a}
+        {this.state.b}
+        ola mundo este es componente bonito, pero hecho con una clase
+      </div>
+    )
+  }
 }
+
+const ModalContentStyled = styled.form`
+  background: var(--bg);
+  color: var(--white);
+  padding: 1.5rem;
+  border-radius: .5rem;
+  position: fixed;
+  inset-block-start: 50%;
+  transform: translateY(-50%) translateX(-50%);
+  inset-inline-start: 50%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  inline-size: 24rem;
+  .title {
+    font: var(--headline2-semi-bold);
+    margin: 0;
+  }
 `
 
 function ModalContent() {
   const form = useRef(null)
   const navigator = useNavigate()
   console.log({ form })
+  const [isActive, setIsActive] = useState(true)
 
   function handleSubmit(event) {
+    setIsActive(false)
     event.preventDefault()
     console.log({ form })
 
@@ -41,13 +81,17 @@ function ModalContent() {
   }
   return (
     <Overlay>
-    <ModalContentStyled ref={form} action="" onSubmit={handleSubmit}>
-      <h2 className='title'>Find an user here:</h2>
-      <InputText type="text" autoComplete='off' name="username" placeholder="Username" />
-      <ButtonContrast text='Buscar'/>
-    </ModalContentStyled>
+      {
+        isActive ? <Modal /> : null
+      }
+      <ModalContentStyled ref={form} action="" onSubmit={handleSubmit}>
+        <h2 className='title'>Busca a tu usuario favorito</h2>
+        <InputText type="text" autoComplete='off' name="username" placeholder="Username" />
+        <ButtonContrast text="Buscar" />
+      </ModalContentStyled>
     </Overlay>
   )
 }
 
 export default ModalContent
+
